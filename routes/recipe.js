fa02bec6ea9ee02544c66a1bd00cc81d3b7624f7
alasaidi <alasaidi55gmail.com>
2024-07-19 12:38:43 +0200

    update user controler

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,13 +1,13 @@
 import express from 'express';
-//import verifyToken from '../middleware/verifyToken.js';
+import verifyToken from '../middleware/verifyToken.js';
 import recipeControllers from '../controllers/recipe.js';
 
 const router = express.Router();
-router.get('/', recipeControllers.getAllRecipes);
-router.post('/', recipeControllers.postRecipe);
-router.get('/:id', recipeControllers.getOneRecipe);
-router.put('/:id', recipeControllers.updateRecipe);
-router.delete('/:id', recipeControllers.deleteRecipe);
+router.get('/', verifyToken, recipeControllers.getAllRecipes);
+router.post('/', verifyToken, recipeControllers.postRecipe);
+router.get('/:id', verifyToken, recipeControllers.getOneRecipe);
+router.put('/:id', verifyToken, recipeControllers.updateRecipe);
+router.delete('/:id', verifyToken, recipeControllers.deleteRecipe);
 
 // routes
 
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,12 @@ import express from 'express';
 import userControllers from '../controllers/user.js';
 
 const router = express.Router();
+const { register, login, logout } = userControllers;
 
 // routes
+
+router.post('/', login);
+router.post('/register', register);
+router.get('/logout', logout);
 
 export default router;
